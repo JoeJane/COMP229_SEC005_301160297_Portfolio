94c9c899a7fe44c670af35fef5f22f170f25c432
JoeJane <jjose139@my.centennialcollege.ca>
2022-03-03 20:52:51 -0500

    updated database configuration details

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-    "URI": "mongodb+srv://admin:ySKoRudHP6wMVvKF@comp229-assignment2.xdrsf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    "URI": "mongodb+srv://admin:ySKoRudHP6wMVvKF@comp229-assignment2.xdrsf.mongodb.net/PortfolioDB?retryWrites=true&w=majority"
 }
